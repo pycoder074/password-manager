62173b119647ff58bc94fceef9596a8b369a11d6
pycoder074 <elliott.kelly08@outlook.com>
2024-03-27 16:38:58 +0000

    PasswordFrame can now be locked by the press of the button

--- passwordFrame.py
+++ passwordFrame.py
@@ -5,11 +5,20 @@ from PyQt6.QtGui import QFont
 class PasswordFrame(QTableWidget):
     def __init__(self, locked: bool, passwords: list):
         super().__init__()
-        self.setHorizontalHeaderLabels(["Website", "Username", "Password"])
         self.layout = QVBoxLayout(self)
-        if not locked:
-            self.setStyleSheet("""
-                                        QTableWidget {
+        self.widgets = []
+        if locked:
+            self.lock()
+        else:
+            self.unlock()
+    def unlock(self):
+        self.setShowGrid(True)
+        self.setColumnCount(3)
+        self.setHorizontalHeaderLabels(["Website", "Username", "Password"])
+        for i in self.widgets:
+            i.setParent(None)
+        self.setStyleSheet("""
+                           QTableWidget {
                                         background-color: none;
                                         border: none;
                                         }
@@ -21,32 +30,45 @@ class PasswordFrame(QTableWidget):
                                         color: white;
                                         }
                                         """)
-            data_font = QFont('Roboto', 16)
-            self.setFont(data_font)
-            self.setColumnCount(3)
-            self.setRowCount(len(passwords))
+        data_font = QFont('Roboto', 16)
+        self.setFont(data_font)
+        self.setColumnCount(3)
+        self.setRowCount(len(passwords))
+        for j, (website, username, password) in enumerate(passwords):
+            website = QTableWidgetItem(website)
+            website.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.setItem(j, 0, website)
+            
+            username = QTableWidgetItem(username)
+            username.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.setItem(j, 1, username)
 
-            for j, (website, username, password) in enumerate(passwords):
-                self.setItem(j, 0, QTableWidgetItem(website))
-                self.setItem(j, 1, QTableWidgetItem(username))
-                self.setItem(j, 2, QTableWidgetItem(password))
+            password = QTableWidgetItem(password)
+            password.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.setItem(j, 2, password)
 
-            self.horizontalHeader().setMinimumSectionSize(200)
-        else:
-            self.setShowGrid(False)
-            self.setStyleSheet("""
-                               QTableWidget{
-                               background-color: #D3D3D3
-                               }""")
-            self.locked_label = QLabel('Passwords are locked \U0001F512')
-            self.layout.addWidget(self.locked_label)
-            self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            self.unlock_button = QPushButton('Unlock Passwords')
-            self.unlock_button.setStyleSheet("""
-                                             QPushButton {
-                                             background-color: #C0C0C
-                                             }""")
-            self.layout.addWidget(self.unlock_button)
+
+        self.horizontalHeader().setMinimumSectionSize(200)
+    
+    def lock(self):
+        self.setShowGrid(False)
+        self.setStyleSheet("""
+                            QTableWidget {
+                           background-color: #D3D3D3
+                            }""")
+        self.locked_label = QLabel('Passwords are locked \U0001F512')
+        self.widgets.append(self.locked_label)
+        self.layout.addWidget(self.locked_label)
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.unlock_button = QPushButton('Unlock Passwords')
+        self.unlock_button.setStyleSheet("""
+                                            QPushButton {
+                                            background-color: #CCCCCC
+                                            }""")
+        self.unlock_button.clicked.connect(self.unlock)
+        self.layout.addWidget(self.unlock_button)
+        self.widgets.append(self.unlock_button)
+
 if __name__ == '__main__':
     app = QApplication([])
     window = QMainWindow()
