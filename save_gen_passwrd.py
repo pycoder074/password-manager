66266db7576036ca95cb85b7d246d4ec4aef4217
pycoder74 <elliott.kelly08@outlook.com>
2024-04-14 11:52:51 +0100

    changed secrets to just have one secret storing all data needed for firebase

--- save_gen_passwrd.py
+++ save_gen_passwrd.py
@@ -1,24 +1,24 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
-import getpass
-from encryption import encrypt
 import os
-def save_gen_passwrd(passwrd, website = None, username = None):
-    cred = os.environ.get("SERVICE_ACCOUNT_KEY")
-    print(cred)
-    cred = credentials.Certificate(cred)
+import json
+import getpass
+from encryption import encrypt  # Make sure to import encrypt from your encryption module
+
+def save_gen_passwrd(passwrd, website=None, username=None):
+    cred_json = os.environ.get("SERVICE_ACCOUNT_KEY")
+    cred_dict = json.loads(cred_json)
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     doc_ref = db.collection('generated_passwords').document()
-    doc_ref.set(
-        {
-        "website" : website,
-        "username" : username,
-        "passwrd" : passwrd
-    }
-    )
+    doc_ref.set({
+        "website": website,
+        "username": username,
+        "passwrd": passwrd
+    })
     print('Generated password saved')
-    
+
 if __name__ == '__main__':
     website = input('Website: ')
     username = input('Username: ')
